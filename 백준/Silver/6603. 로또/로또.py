b39eabaccffb4dd920396c893261e80c5b5de997
Seoyeon Choi <71605276+seoyeonchoi@users.noreply.github.com>
2023-04-23 10:54:21 +0900

    [Silver II] Title: 로또, Time: 176 ms, Memory: 117868 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,24 +1,24 @@
-def nCr(n, ans, r):
-    if n == len(nums):
-        if len(ans) == r:
-            temp = [i for i in ans]
-            answer_list.append(temp)
+def combi(level, start):  # 조합 함수
+    if level == K:  # 조합 탈출 조건
+        for a in arr: 
+            print(a, end=' ') # 정답 원소들 하나씩 출력 
+        print()  # 줄바꿈
         return
-    ans.append(nums[n])
-    nCr(n + 1, ans, r)
-    ans.pop()
-    nCr(n + 1, ans, r)
     
+    for i in range(start, N-K+level+1):  # 각 레벨에서 고려해야 하는 만큼의 range 
+        arr[level] = nums[i]  # 현재 레벨에서 고려한 단어 append
+        combi(level+1, i+1)  # 그 다음 레벨 순회를 위한 재귀
+
 while True:
     nums = list(map(int, input().split()))
-    answer_list = []
     
     if nums[0] == 0:
         break
     else:
-        nCr(1, [], 6)
-        for answer in answer_list:
-            for a in answer:
-                print(a, end=' ')
-            print()
+        N = nums[0]  # 고려해야하는 깊이
+        K = 6  # 뽑을 수
+        nums = nums[1:]
+        answer_list = []
+        arr = [0] * K
+        combi(0, 0)
         print()
