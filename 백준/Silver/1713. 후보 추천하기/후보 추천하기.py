4c674d2de96694d447af6238d2956818d39c282a
Seoyeon Choi <71605276+seoyeonchoi@users.noreply.github.com>
2023-07-16 18:25:06 +0900

    [Silver I] Title: 후보 추천하기, Time: 216 ms, Memory: 116264 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -0,0 +1,23 @@
+from collections import defaultdict, deque
+
+n = int(input())
+m = int(input())
+photos = defaultdict(lambda: 0)
+
+recommend_list = deque(map(int, input().split()))
+
+while recommend_list:
+    ready = recommend_list.popleft()
+    if ready in photos:
+        photos[ready] += 1
+    else:
+        if len(photos) == n:
+            keys = list(photos.keys())
+            values = list(photos.values())
+            min_val = values.index(min(values))
+            idx = keys[min_val]
+            del(photos[idx])
+        
+        photos[ready] = 1
+        
+print(" ".join(list(map(str, sorted(photos.keys())))))
